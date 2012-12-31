4847c577cd5ad93e8ab679cef61767a950b1f453
Bart Romgens <bart.romgens@gmail.com>
2012-12-31 13:31:19 +0100

    Added signal to update the drink mass or volume based on user input.

--- glass/models.py
+++ glass/models.py
@@ -1,11 +1,13 @@
-from django.db import models
+from django.db.models import signals
 
+from django.db import models
 from bottle.models import Bottle
 from userprofile.models import UserProfile
 
 class Glass(models.Model):
   bottle = models.ForeignKey(Bottle)
-  volume = models.FloatField('volume')
+  mass = models.FloatField('mass', default=0.0, blank=True)
+  volume = models.FloatField('volume', default=0.0)
   user = models.ForeignKey(UserProfile)
   rating = models.IntegerField(default=-1, blank=True)
 
@@ -17,3 +19,21 @@ class Glass(models.Model):
   
   def __unicode__(self):
     return str(self.volume) + '[ml] of ' + str(self.bottle)
+  
+
+def updateMassAndVolume(sender, **kwargs):
+  drink = kwargs["instance"]
+  
+  alcoholPer = drink.bottle.whisky.alcoholPercentage
+  waterPer = 100 - alcoholPer
+  density = (1.0*waterPer + 0.789*alcoholPer) / 100.0
+  
+  if kwargs["created"]:
+    if (drink.mass > 0):
+      drink.volume = drink.mass * 1.0/density
+      drink.save()
+    else:
+      drink.mass = drink.volume * density
+      drink.save()
+
+signals.post_save.connect(updateMassAndVolume, sender=Glass, dispatch_uid="drink_creation_signal")
