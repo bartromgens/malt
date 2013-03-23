b0eda45f19437de3aba27025af57eaf5448f6eb2
Bart Romgens <bart.romgens@gmail.com>
2013-03-23 13:48:17 +0100

    Added missing include.

--- whisky/models.py
+++ whisky/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from datetime import datetime
+
 class Region(models.Model):
   name = models.CharField(max_length=200)
   lat = models.FloatField('latitude', default='0.0')
