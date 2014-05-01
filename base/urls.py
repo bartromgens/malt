8ae66bbb947b6a91506dc2c7327944e62d68ca4b
Bart Romgens <bart.romgens@gmail.com>
2014-05-01 22:49:27 +0200

    Changed root page to collection page.

--- base/urls.py
+++ base/urls.py
@@ -2,13 +2,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
 from django.contrib import admin
 from base.views import HomeView, AboutView, HelpView, EventsView, EventView, TestView
+from bottle.views import StockView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-  url(r'^$', login_required(HomeView.as_view())),
+#   url(r'^$', login_required(HomeView.as_view())),
+  url(r'^$', login_required(StockView.as_view())),
   url(r'^test/$', login_required(TestView.as_view())),
   url(r'^login/$', 'base.views.login'),
   url(r'^logout/$', 'base.views.logout'),
