cc78e37f5e360ea5bf58db8ceef7dd4d5ea8e505
Bart Römgens <bart.romgens@gmail.com>
2015-05-14 13:01:29 +0200

    Remove STATIC_ROOT and STATIC_URL from settings
    These are defined in local settings

--- base/settings.py
+++ base/settings.py
@@ -54,18 +54,6 @@ MEDIA_ROOT = ''
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-#STATIC_ROOT = '/home/bartromgens/webapps/maltstatic/'
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-#STATIC_URL = 'http://www.malttracker.com/maltstatic/'
-STATIC_URL = APP_DIR + 'static/'
-
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
